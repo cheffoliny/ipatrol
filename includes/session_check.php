7f8603bd30cc171aaeb9d1389e4c229d6374a321
cheffoliny <cheffoliny@email>
2025-11-17 19:23:57 +0200

    GEO TRACE TEST PLAY SOUND ON ALARM - STOP SOUND

--- includes/session_check.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Проверка дали потребителят е логнат
-if (!isset($_SESSION['user_id'])) {
-    header('Location: index.php');
-    exit;
-}
--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'session_init.php';
-session_unset();
-session_destroy();
-header('Location: index.php');
-exit;
